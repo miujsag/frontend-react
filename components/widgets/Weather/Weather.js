4516930f9722392828e7946b937960de12c097bb
László Horváth <bedafoltyn@gmail.com>
2020-05-09 18:49:01 +0200

    weather widget modified to fit the new model

--- components/widgets/Weather/Weather.js
+++ components/widgets/Weather/Weather.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function round (number) {
-  return number ? Math.round(number) : ''
+function round(number) {
+  return number ? Math.round(number) : "";
 }
 
-export default function Weather ({weather}) {
+export default function Weather({ weather }) {
   return (
     <div className="weather" title={weather.description}>
-      <p>{round(weather.currentTemperature)}°C</p>
+      <p>{round(weather.current_temperature)}°C</p>
       <img src={`images/weather/${weather.icon}.svg`} alt="időjárás ikon" />
     </div>
-  )
+  );
 }
